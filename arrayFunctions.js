4883477582086580c184d6d7027749d0538356f2
Ronnie Legaspi <ronnielegaspi98@gmail.com>
2026-07-21 00:19:45 +0800

    Array Functions

---
+++ arrayFunctions.js
@@ -0,0 +1,13 @@
+// Array funcitons 
+let arr = ["Ronnie", "Hortizuela", "Legaspi"]
+// push() add item in the last postion 
+arr.push("test")
+// unshift() - adding item to the first slot
+arr.unshift("first")
+// shift() - deletes first item 
+arr.shift()
+// pop() - delete last Item
+arr.pop()
+// join() - joines all item with separated customizable string
+arr.join(", ")
+arr
