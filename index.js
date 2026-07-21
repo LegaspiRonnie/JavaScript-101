dfaabb22e42af2c35fa9224bcbbabb263ce858a0
Ronnie Legaspi <ronnielegaspi98@gmail.com>
2026-07-21 19:21:34 +0800

    complete promises

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-let name = "Ronnie"
-name
+const fetch = () => {
+    const result = axios({
+        url: 'https://jsonplaceholder.typicode.com/posts', 
+        method: "GET"
+
+    }).then((fromResolve) => {
+        return fromResolve
+    }).catch((fromReject) =>{
+        return fromReject
+    })
+
+    return result
+}
+
+fetch().then((data) => {
+    console.log(data)
+})
